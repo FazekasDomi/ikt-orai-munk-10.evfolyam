de001f03dc2a697322d8053aabf3252634c30658
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2022-10-20 12:14:01 +0200

    Add files via upload

---
+++ pyton/oyton.py
@@ -0,0 +1,34 @@
+#1-2.feladat
+'''
+szam1 = int(input("kérek egy számot"))
+szam2 = int(input("kérek egy másik számot"))
+szam3 = int(input("kérek megint egy másik számot"))
+if szam1>szam2 and szam1>szam3
+    print (szam1, "a legnagyobb")
+else szam2>szam1 and szam2>szam3
+    print (szam2, "a legnagyobb")
+else szam3>szam2 and szam3>szam1
+    print (szam3, "a legnagyobb")'''
+
+#3.feladat
+'''
+pontszam = int(input("kérek egy pontszámot"))
+if pontszam<50
+    print("sajnélom de eggyes")
+else 50<=pontszam<60
+    print("legalább kettes")
+else 60<=pontszam<70
+    print("legalább hármas")
+else 70<=pontszam<85
+    print("ügyes! négyes")
+else 85<=pontszam
+    print("gratulálok! ötös")'''
+#6.feladat
+'''
+szam1 = int(input("kérek egy számot"))
+szam2 = int(input("kérek egy másik számot"))
+szam3 = int(input("kérek megint egy másik számot"))
+if szam1%2==0 and szam2%2==0 and szam3%2==0
+     print("párosak")
+else:
+     print("van benne nem páros")'''
