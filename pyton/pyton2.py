4f5526ec14caef22f5715edadd9883ebf0664d2c
FazekasDomi <112930880+FazekasDomi@users.noreply.github.com>
2022-11-17 13:09:23 +0100

    Add files via upload

--- pyton/pyton2.py
+++ pyton/pyton2.py
@@ -1,4 +1,5 @@
 #1a
+'''
 szam = 20
 while szam >= 1:
     print(szam)
@@ -10,3 +11,14 @@ szam = szam - 1
 while szam >= 1:
     print(szam)
     szam = szam - 2
+'''
+#6
+'''
+import random
+gondolt_szam: random.randint (1,10)
+tipp= int(input("mondj számot"))
+if gondolt_szam==tipp:
+    print ("yep")
+else:
+    print("nop")
+'''
